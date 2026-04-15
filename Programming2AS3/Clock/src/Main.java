bb421c004946d5ad4ab9dd8b789435b4f6a75a64
Prason KC <prasonkc07@gmail.com>
2026-04-15 10:07:22 +0545

    .

--- Programming2AS3/Clock/src/Main.java
+++ Programming2AS3/Clock/src/Main.java
@@ -1,29 +1,63 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-class Clock{
+/**
+ * Maintains the current date and time.
+ * 
+ * @author Prason KC
+ */
+class Clock {
+    /**
+     * Stores the most recently captured date and time value.
+     */
     private volatile LocalDateTime dateTime;
 
-    Clock(){
+    /**
+     * Creates a clock initialized with the current local date and time.
+     */
+    Clock() {
         this.dateTime = LocalDateTime.now();
     }
 
-    void displayDateTime(){
+    /**
+     * Displays the stored date and time in the console.
+     */
+    void displayDateTime() {
+        // Formats the time in HH:mm:ss dd-MM-yyyy format.
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
         System.out.print("\r Current Time and Date: " + "[" + dateTime.format(formatter) + "]");
     }
 
-    void updateDateTime(){
+    /**
+     * Updates the date and time variable.
+     */
+    void updateDateTime() {
         dateTime = LocalDateTime.now();
     }
 }
 
-class Main{
-    public  static void main(String[] args) {
+/**
+ * Starts the clock application.
+ *
+ * @author Prason KC
+ */
+class Main {
+    /**
+     * Launches the clock application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * Coordinates access to the current date and time value.
+         */
         Clock clock = new Clock();
-    
+
+        /**
+         * Refreshes the stored time every 200 ms.
+         */
         Thread updateThread = new Thread(() -> {
-            while (true){
+            while (true) {
                 try {
                     clock.updateDateTime();
                     Thread.sleep(200);
@@ -33,8 +67,11 @@ class Main{
             }
         });
 
+        /**
+         * Prints the current time to the console every second.
+         */
         Thread displayThread = new Thread(() -> {
-            while (true){
+            while (true) {
                 try {
                     clock.displayDateTime();
                     Thread.sleep(1000);
